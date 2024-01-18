ec6a28d139cb2e4f20a5d9462f334034e32456d0
Vadym Karachenko <v1dr00x@gmail.com>
2024-01-18 01:15:47 -0800

    Fix disappearing logo on Safari and incorrect height of header content on small screens

--- app/src/components/cutcutfilm-logo/cutcutfilm-logo.tsx
+++ app/src/components/cutcutfilm-logo/cutcutfilm-logo.tsx
@@ -36,8 +36,8 @@ const CutcutfilmLogoContent = ({
 	<svg
 		xmlns='http://www.w3.org/2000/svg'
 		height={height}
+		width={width}
 		fill='none'
-		style={{ maxWidth: width, maxHeight: height, height: '100%', width: '100%' }}
 		viewBox={`0 0 ${width} ${height}`}
 		{...rest}>
 		<path
